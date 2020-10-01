72cb4929356263850376794458fd8085ec729945
Martin <martin@entail.no>
2020-10-01 09:22:44 +0200

    Fikset mangler

--- latexreport/__init__.py
+++ latexreport/__init__.py
@@ -4,7 +4,7 @@
 
 
 from .get_documents import get_document
-from .make_documents import make_document
+from .make_documents import make_latex_document, make_word_document
 
 __version__ = '0.1'
 __all__ = ['get_document', 'make_document', 'utils']
